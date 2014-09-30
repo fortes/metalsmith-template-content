326a9ee154145e1433bd3d11e7752e83a36720ac
Filipe Fortes <accounts@fortes.com>
2014-09-30 07:39:53 -0700

    Initial checkin

---
+++ README.md
@@ -0,0 +1,13 @@
+# metalsmith-template-content
+
+Use the template language of your choice within the content of an article
+
+## Usage
+
+```js
+var content_templates = require('metalsmith-content-templates');
+
+metalsmith.use({
+  engine: 'swig'
+});
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+var async = require('async'),
+    consolidate = require('consolidate'),
+    extend = require('extend'),
+    path = require('path');
+
+/**
+ * @param {!Object} data
+ * @param {!Metalsmith} metalsmith
+ * @param {!Object} options
+ * @param {!Function} callback
+ */
+var renderArticle = function(data, metalsmith, options, callback) {
+  console.log(metalsmith.metadata());
+  consolidate[options.engine].render(
+    data.contents.toString(),
+    extend({}, options, metalsmith.metadata(), data),
+    function(error, result) {
+      if (error) {
+        return callback(error);
+      }
+      console.log(result);
+      data.contents = result;
+      callback();
+    }
+  );
+};
+
+module.exports = function(options) {
+  if (!options || !options.engine) {
+    throw new Error('Must specify engine');
+  }
+
+  if (!(options.engine in consolidate)) {
+    throw new Error('Invalid engine name: ' + options.engine);
+  }
+
+  /**
+   * @param {Object} files
+   * @param {Metalsmith} metalsmith
+   * @param {Function} done
+   */
+  return function(files, metalsmith, done) {
+    var articles = Object.keys(files).filter(function(file) {
+          // Only operate on HTML or Markdown
+          return /\.(html|md)$/.test(path.extname(file)) && /archive/.test(file);
+        });
+
+    async.each(articles, function(file, callback) {
+      renderArticle(files[file], metalsmith, options, callback);
+    }, done);
+  };
+};
